f885459cd30c0b0854c557873e2d5fbd2ef8970d
Adam Lynch <adamlynchmob@gmail.com>
2019-07-25 22:31:26 -0400

    started work on the minibar drawer

---
+++ src/components/Delivery.vue
@@ -0,0 +1,35 @@
+<template>
+  <drawer>
+    <h1 slot="header">Home alcohol delivery services (2 available)</h1>
+    <div slot="content">
+
+    </div>
+  </drawer>
+</template>
+
+<script>
+import Drawer from './Drawer.vue'
+import axios from 'axios'
+export default {
+  name: 'Delivery',
+  components: {
+    Drawer
+  },
+  created () {
+    // 42.404754, -71.116237
+    axios.get('', {
+      headers: {
+        // TODO
+      }
+    })
+    axios.get('https://minibardelivery.com/api/v2/suppliers', {
+      params: {
+        coords: {
+          lat: 42.404754,
+          lng: -71.116237
+        }
+      }
+    })
+  }
+}
+</script>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="drawer">
+    <slot name="header"/>
+    <div class="content">
+      <slot name="content"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Drawer'
+}
+</script>
